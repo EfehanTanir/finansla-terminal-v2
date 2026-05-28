23ecd54809b27467542fc1203bba0ccab8a692ad
Efehan Tanırgan <efehantanirgan7@gmail.com>
2026-05-28 00:37:52 +0000

    fixed again

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,4 @@
 from .api_clients import StockDataClient, NewsClient
-from .cache import cached_request
 from .charts import create_candlestick_chart, create_price_chart
 from .sentiment import analyze_sentiment
 from .helpers import format_currency, format_percent, calculate_change
@@ -7,7 +6,6 @@ from .helpers import format_currency, format_percent, calculate_change
 __all__ = [
     'StockDataClient',
     'NewsClient',
-    'cached_request',
     'create_candlestick_chart',
     'create_price_chart',
     'analyze_sentiment',
